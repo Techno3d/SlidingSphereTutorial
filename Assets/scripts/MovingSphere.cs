c98202b136b91333077f75fb9c0c5721e738e05a
shadman syed <syeds1>
2024-09-30 13:17:34 -0400

    First commit

---
+++ Assets/scripts/MovingSphere.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxAccel = 10f;
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInp = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 desired = new Vector3(playerInp.x, 0f, playerInp.y);
+        desired = Vector3.ClampMagnitude(desired, 1f);
+        desired *= Time.deltaTime * maxAccel;
+
+        //Actual vel
+        Vector3 velocity = new Vector3(0f, 0f, 0f);
+        velocity.x = Mathf.MoveTowards(velocity.x, desired.x, maxAccel*Time.deltaTime);
+        velocity.z = Mathf.MoveTowards(velocity.y, desired.y, maxAccel*Time.deltaTime);
+        transform.localPosition += velocity * Time.deltaTime;
+    }
+}
