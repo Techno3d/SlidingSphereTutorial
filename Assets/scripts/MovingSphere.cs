2b3413389c3c38c651a4c0c902eef1b48078850d
Shadman Syed <shadman3d@gmail.com>
2024-10-03 20:34:23 -0400

    Finish catlikecoding tutorial

--- Assets/scripts/MovingSphere.cs
+++ Assets/scripts/MovingSphere.cs
@@ -10,6 +10,8 @@ public class MovingSphere : MonoBehaviour
     float maxSpeed = 10f;
     [SerializeField]
     Rect bounding = new Rect(-5f, -5f, 10f, 10f);
+    [SerializeField, Range(0f, 1f)]
+	float bounciness = 0.5f;
     Vector3 velocity = new Vector3(0f, 0f, 0f);
     // Start is called before the first frame update
     void Start()
@@ -28,6 +30,17 @@ public class MovingSphere : MonoBehaviour
         //Actual vel
         velocity.x = Mathf.MoveTowards(velocity.x, desired.x, maxAccel*Time.deltaTime);
         velocity.z = Mathf.MoveTowards(velocity.z, desired.z, maxAccel*Time.deltaTime);
-        transform.localPosition += velocity * Time.deltaTime;
+        Vector3 newPos = transform.localPosition + velocity * Time.deltaTime;
+        if(newPos.x < bounding.xMin || newPos.x > bounding.xMax) {
+            velocity.x *= -1*bounciness;
+        }
+        if(newPos.z < bounding.yMin || newPos.z > bounding.xMax) {
+            velocity.z *= -1*bounciness;
+        }
+        if(!bounding.Contains(new Vector2(newPos.x, newPos.z))) {
+            newPos.x = Mathf.Clamp(newPos.x, bounding.xMin, bounding.xMax);
+            newPos.z = Mathf.Clamp(newPos.z, bounding.yMin, bounding.yMax);
+        }
+        transform.localPosition = newPos;
     }
 }
